f5a1c78c03134563f4000b1583b6e67ec5bdd462
IJ Roy <141866152+ij-roy@users.noreply.github.com>
2023-10-05 19:20:01 +0530

    Update and rename question 4.py to question 04.py

--- question 04.py
+++ question 04.py
@@ -4,7 +4,11 @@ Created on Fri Aug 25 15:25:53 2023
 
 @author: LAB
 """
-
+'''
+4. Take a 3 digit number as input and find the sum of its digits. Also, check if the sum is divisible by
+3 or not.
+'''
+#incomplete 
 a = int(input('type 3 digit number:'))
 q = a%10
 w = a//10
