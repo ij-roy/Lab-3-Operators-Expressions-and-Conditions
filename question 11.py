f52dea33db11c42811f84a3d551e3c564c35b4ec
IJ Roy <141866152+ij-roy@users.noreply.github.com>
2023-10-05 19:32:44 +0530

    Update question 11.py

--- question 11.py
+++ question 11.py
@@ -4,7 +4,9 @@ Created on Fri Aug 25 16:35:42 2023
 
 @author: LAB
 """
-
+'''
+11. Take a 3 digit number as input. Check if it is an Armstrong number or not. E.g. 13 + 53 + 33 = 153
+'''
 a = int(input('type three digit number:'))
 q=a%10
 w=a//10
