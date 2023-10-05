8d8dea8dde60561f8cccedfeb77b35e49bc949e2
IJ Roy <141866152+ij-roy@users.noreply.github.com>
2023-10-05 19:25:55 +0530

    Update and rename question 6.py to question 06.py

--- question 06.py
+++ question 06.py
@@ -4,7 +4,10 @@ Created on Fri Aug 25 15:57:02 2023
 
 @author: LAB
 """
-
+'''
+6. Swap the values of two integer variables without using a third variable or multiple assignment
+operation.
+'''
 a = int(input('enter the value of a:'))
 b = int(input('enter the value of b:'))
 
