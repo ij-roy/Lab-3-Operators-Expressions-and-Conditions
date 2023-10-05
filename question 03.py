3e9e91acdd368356bcc8a70676803aa44936da7c
IJ Roy <141866152+ij-roy@users.noreply.github.com>
2023-10-05 19:17:46 +0530

    Update and rename question 3.py to question 03.py

--- question 03.py
+++ question 03.py
@@ -4,7 +4,10 @@ Created on Fri Aug 25 15:19:42 2023
 
 @author: LAB
 """
-
+'''
+3. Take 3 numbers as input, and find whether they form the sides of a triangle or not. Consider
+invalid cases also
+'''
 a = float(input('1st number:'))
 b = float(input('2nd number:'))
 c = float(input('3rd number:'))
